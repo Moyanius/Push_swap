92dd9842fb78cc894bef590d48423c14fe2f31c3
Moyanius <moyanojimenezjose@gmail.com>
2022-07-04 18:38:43 +0200

    42 cursus

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jmoyano- <jmoyano-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 20:12:28 by jmoyano-          #+#    #+#             */
-/*   Updated: 2022/06/28 20:29:15 by jmoyano-         ###   ########.fr       */
+/*   Updated: 2022/07/04 18:38:25 by jmoyano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@
 # include <string.h>
 # include <strings.h>
 # include "./Moyano_library/libft.h"
+
+typedef struct s_list {
+	int value;
+	struct node* next;
+} t_list;
 
 int		push_swap(int total_arg, char **nums);
 int main(int agrc , char **agrv);
