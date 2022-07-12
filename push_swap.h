d832cc636beee254522b3ec470284e5cf7cbc0ae
Moyanius <moyanojimenezjose@gmail.com>
2022-07-12 19:21:11 +0200

    42 cursus

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jmoyano- <jmoyano-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 20:12:28 by jmoyano-          #+#    #+#             */
-/*   Updated: 2022/07/12 19:17:24 by jmoyano-         ###   ########.fr       */
+/*   Updated: 2022/07/12 19:20:56 by jmoyano-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ void	num_validation(int total_arg, char **nums);
 int		ft_isdigit_pushswap(char *str);
 int		ft_atoi(const char *str);
 void	push_swap_errors(int c);
+void	duplicate_validation(char **nums);
 
 #endif
